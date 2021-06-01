9c6ec48c21dcd4b37a59d697230b3fec2b4f7c9c
Pratham <bhagatpratham101@gmail.com>
2021-06-01 20:46:27 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,43 +1,66 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Profile = () => {
-  return (
-    <div>
-      <section>
-        <div className='github-profile-info'>
-          <a className='avatar'>
-            <img></img>
-          </a>
-          <h2 className='Username'>
-            <a>Name</a>
-          </h2>
-          <h3 className='Location'>Location</h3>
-        </div>
-        <div className='github-profile-stats'>
-          <ul>
-            <li>
-              <a>
-                <i></i>
-                <span>Followers</span>
+const Profile = ({ data }) => {
+  let followers = `${data.homeUrl}/followers`;
+  let repositories = `${data.homeUrl}?tab=repositories`;
+  let following = `${data.homeUrl}/following`;
+
+  if (data.notFound === "Not Found")
+    return (
+      <div className='notfound'>
+        <h2>Oops !!!</h2>
+        <p>The Component Couldn't Find The You Were Looking For . Try Again </p>
+      </div>
+    );
+  else
+    return (
+      <div>
+        <section>
+          <div className='github-profile-info'>
+            <a
+              href={data.homeUrl}
+              target='_blank'
+              title={data.name || data.username}
+            >
+              <img src={data.avatar} alt={data.username} />
+            </a>
+
+            <h2 className='Username'>
+              <a href={data.homeUrl} title={data.username} target='_blank'>
+                {data.name || data.username}
               </a>
-            </li>
-            <li>
-              <a>
-                <i></i>
-                <span>Repositories</span>
-              </a>
-            </li>
-            <li>
-              <a>
-                <i></i>
-                <span>Following</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-    </div>
-  );
+            </h2>
+            <h3 className='Location'>{data.location}</h3>
+          </div>
+          <div className='github-profile-stats'>
+            <ul>
+              <li>
+                <a href={followers} target='_blank' title='Number Of Followers'>
+                  <i>{data.followers}</i>
+                  <span>Followers</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href={repositories}
+                  target='_blank'
+                  title='Number Of Repositoriy'
+                >
+                  <i>{data.repos}</i>
+                  <span>Repositories</span>
+                </a>
+              </li>
+              <li>
+                <a href={following} target='_blank' title='Number Of Following'>
+                  <i>{data.following}</i>
+                  <span>Following</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
 };
 
 export default Profile;
